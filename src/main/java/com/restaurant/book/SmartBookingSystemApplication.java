3e5c4939d665e97b26f2532abbd70082f5a98604
ander.saarniit <ander.saarniit@ut.ee>
2026-02-27 18:57:04 +0200

    initial commit

---
+++ src/main/java/com/restaurant/book/SmartBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBookingSystemApplication.class, args);
+    }
+
+}
