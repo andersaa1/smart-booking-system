caced22defe8566a5352a80751bb3b4cea666a22
ander.saarniit <ander.saarniit@ut.ee>
2026-03-11 11:26:07 +0200

    Created a custom button component that can be reused across components (#6)

---
+++ frontend/src/components/CustomButton.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+type Props = {
+  label: string;
+  onClick?: () => void;
+};
+
+export default function CustomButton({ label, onClick }: Props) {
+  return;
+  <button className="button" onClick={onClick}>
+    {label}
+  </button>;
+}
